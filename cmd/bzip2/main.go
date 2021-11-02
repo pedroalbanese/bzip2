021122bdce3b31c28eee5fcc310c0fdd0604e80b
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-11-02 20:35:54 -0300

    Multicore

--- cmd/bzip2/main.go
+++ cmd/bzip2/main.go
@@ -1,7 +1,6 @@
 // Copyright (c) 2021, Pedro Albanese. All rights reserved.
 // Use of this source code is governed by a ISC license that 
 // can be found in the LICENSE file.
-
 package main
 
 import (
